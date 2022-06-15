b3551760bc19b03a90bcca30e3c267a57984adfd
Nui Narongwet <narongwet.m@gmail.com>
2022-06-15 22:46:53 +0700

    Move docker context output to context directory

--- echo-build.zsh
+++ echo-build.zsh
@@ -25,7 +25,7 @@ echo_build() {
     distro=$1
     version=$2
     image=nmk-vendor:$distro-$version
-    echo docker build --pull -t $image $distro-$version
+    # echo docker build --pull -t $image $distro-$version
     echo docker run -v $PWD/workspace:/workspace $image --name $distro-${version}_tmux-3.3a /nmk-vendor
     # echo docker rmi $image
 }
